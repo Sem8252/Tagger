9ed214ec1cfcc6f91f7f481fa667c62321b41835
Semyon Arkhipov <sem8252@yandex.ru>
2018-12-25 16:00:23 +0300

    123

--- Tagger/FileProcessor.cs
+++ Tagger/FileProcessor.cs
@@ -10,42 +10,123 @@ namespace Tagger
 {
     class FileProcessor
     {
-        public static List<FileInfo> scanDirectories(string path)
+        public static List<FileInfo> ScanDirectories(string path, bool isSubDirectories)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
             List<FileInfo> files = new List<FileInfo>();
-            addArrayToList(directory.GetFiles(), files);
-            foreach(var current in directory.GetDirectories("*.*", SearchOption.AllDirectories))
-            {
-                addArrayToList(current.GetFiles(), files);
-            }
+            AddFilesToList(directory.GetFiles(), files);
+            if (isSubDirectories)
+                foreach (var current in directory.GetDirectories("*.*", SearchOption.AllDirectories))
+                {
+                    AddFilesToList(current.GetFiles(), files);
+                }
             return files;
         }
 
-        public static void giveNumbers(List<FileInfo> files)
+        public static List<string> GetTagsFromDirectory(List<FileInfo> files)
         {
-            string extention;
-            int number;
+            List<string> tags = new List<string>();
             foreach(var file in files)
             {
-                extention = file.Name.Split('.')[1];
-                try
+                var currentTags = GetTagsFromFile(file);
+                foreach(var current in currentTags)
                 {
-                    number = Convert.ToInt32(file.Name.Split('.')[0].Split('%')[0]);
+                    if (!tags.Contains(current))
+                        tags.Add(current);
                 }
-                catch
-                {
+            }
+            return tags;
+        }
 
+        public static List<string> GetTagsFromFile(FileInfo file)
+        {
+            var nameAndType = file.Name.Split('.');
+            var tags = nameAndType[0].Split('%').ToList();
+            tags.RemoveAt(0);
+            var distinct = tags.Distinct().ToList();
+            return distinct;
+        }
+
+        public static void AddTag(List<FileInfo> files, string tag)
+        {
+            foreach(var file in files)
+            {
+                var tags = GetTagsFromFile(file);
+                if (!tags.Contains(tag))
+                {
+                    tags.Add(tag);
+                    var name = file.FullName.Split('.')[0].Split('%')[0];
+                    var res = file.FullName.Split('.')[1];
+                    StringBuilder sb = new StringBuilder();
+                    tags.ForEach(x => sb.Append('%' + x));
+                    var newName = string.Format(name + sb + '.' + res);
+                    file.CopyTo(newName);
+                    file.Delete();
                 }
             }
         }
 
-        private static void addArrayToList(FileInfo[] array, List<FileInfo> list)
+        public static void RemoveTag(List<FileInfo> files, string tag)
         {
-            foreach(var file in array)
+            foreach (var file in files)
+            {
+                var tags = GetTagsFromFile(file);
+                if (tags.Contains(tag))
+                {
+                    tags.Remove(tag);
+                    var name = file.FullName.Split('%')[0];
+                    var res = file.Name.Split('.')[1];
+                    StringBuilder sb = new StringBuilder();
+                    tags.ForEach(x => sb.Append('%' + x));
+                    var newName = string.Format(name + sb + '.' + res);
+                    file.CopyTo(newName);
+                    file.Delete();
+                }
+            }
+        }
+
+        private static void AddFilesToList(FileInfo[] files, List<FileInfo> list)
+        {
+            foreach(var file in files)
             {
                 list.Add(file);
             }
         }
+
+        //public static bool isCopyNames(List<FileInfo> files)
+        //{
+        //    var checkedFiles = files;
+        //    foreach(var file in checkedFiles)
+        //    {
+        //        //bool isCopy = false;
+        //        var tags = GetTagsFromFile(file);
+        //        foreach(var tag in tags)
+        //        {
+        //            if (tag.Contains("(%)"))
+        //            {
+        //                //isCopy = true;
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //public static void ReplaceCopyNames(List<FileInfo> files)
+        //{
+        //    foreach (var file in files)
+        //    {
+        //        var tags = GetTagsFromFile(file);
+        //        var corruptedTag = tags.FindIndex(x => x.Contains("("));
+        //        tags[corruptedTag] = tags[corruptedTag].Split(' ')[0];
+        //        var name = file.FullName.Split('%')[0];
+        //        var res = file.Name.Split('.')[1];
+        //        StringBuilder sb = new StringBuilder();
+        //        tags.ForEach(x => sb.Append('%' + x));
+        //        var newName = string.Format(name + sb + '.' + res);
+        //        file.CopyTo(newName);
+        //        file.Delete();
+        //    }
+        //}
     }
 }
--- Tagger/MainWindow.xaml.cs
+++ Tagger/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Tagger
 {
@@ -23,6 +25,115 @@ namespace Tagger
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected string path;
+        protected List<FileInfo> files = new List<FileInfo>();
+        protected List<string> tags = new List<string>();
+        protected FolderBrowserDialog browser = new FolderBrowserDialog();
+        protected static Random rand = new Random();
+        protected const string characters = "qwertyuiopasdfghjklzxcvbnm0123456789";
+
+        private void ButtonPath_Click(object sender, RoutedEventArgs e)
+        {
+            browser.ShowDialog();
+            path = browser.SelectedPath;
+            PathLabel.Content = path;
+            Rescan();
+        }
+
+        private void AddTagButton_Click(object sender, RoutedEventArgs e)
+        {
+            Rescan();
+            if (!TextBoxTag.Text.Contains('%'))
+                FileProcessor.AddTag(files, TextBoxTag.Text);
+            Rescan();
+        }
+
+        private void DeleteTagButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Rescan();
+            FileProcessor.RemoveTag(files, TagListComboBox.SelectionBoxItem.ToString());
+            Rescan();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            Rescan();
+        }
+
+        protected void Rescan()
+        {
+            TagListComboBox.Items.Clear();
+            files = FileProcessor.ScanDirectories(path, CheckBoxSubDirectory.IsChecked.Value);
+            //if(FileProcessor.isCopyNames(files))
+            //{
+            //    System.Windows.MessageBox.Show("Копия!");
+            //}
+            tags = FileProcessor.GetTagsFromDirectory(files);
+            foreach (var tag in tags)
+                TagListComboBox.Items.Add(tag);
+        }
+
+        private void CheckBoxSubDirectory_Checked(object sender, RoutedEventArgs e)
+        {
+            Rescan();
+        }
+
+        private void CheckBoxSubDirectory_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Rescan();
+        }
+
+        //private void ButtonLarge_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var largeFiles = files.FindAll(x => x.FullName.Contains("large"));
+        //    foreach (FileInfo file in largeFiles)
+        //    {
+        //        int indexOfLarge = file.FullName.IndexOf("large");
+        //        string newName = file.FullName.Remove(indexOfLarge - 1, 6);
+        //        file.CopyTo(newName);
+        //        file.Delete();
+        //    }
+        //}
+
+        private void ButtonFix_Click(object sender, RoutedEventArgs e)
+        {
+            Rescan();
+            var largeFiles = files.FindAll(x => x.FullName.Contains("large"));
+            foreach (FileInfo file in largeFiles)
+            {
+                int indexOfLarge = file.FullName.IndexOf("large");
+                string newName = file.FullName.Remove(indexOfLarge - 1, 6);
+                file.CopyTo(newName);
+                file.Delete();
+            }
+            Rescan();
+            var unusualFiles = files.FindAll(x => x.FullName.Contains("file"));
+            foreach (FileInfo file in unusualFiles)
+            {
+                char[] randomName = new char[10];
+                for (int i = 0; i < randomName.Length; i++)
+                    randomName[i] = characters[rand.Next(characters.Length)];
+                string newName = file.FullName.Replace("file", new string(randomName));
+                file.CopyTo(newName);
+                file.Delete();
+            }
+            Rescan();
+            var copyes = files.FindAll(x => x.FullName.Contains("("));
+            foreach (FileInfo file in copyes)
+            {
+                string newName = file.FullName;
+                int indexOfErr = newName.IndexOf("(");
+                newName = newName.Remove(indexOfErr - 1, 4);
+                char[] randomName = new char[3];
+                for (int i = 0; i < randomName.Length; i++)
+                    randomName[i] = characters[rand.Next(characters.Length)];
+                newName.Insert(0, new string(randomName));
+                file.CopyTo(newName);
+                file.Delete();
+            }
+            Rescan();
         }
     }
 }
