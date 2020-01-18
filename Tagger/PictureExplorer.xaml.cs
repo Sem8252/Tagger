7763297da9c223b051d929b71294aa6026606b15
Надир Надирбеков <nadirbekovnadir@yandex.ru>
2020-01-19 01:56:20 +0300

    Написал комментариев Подкорректил конструктор

--- Tagger/PictureExplorer.xaml.cs
+++ Tagger/PictureExplorer.xaml.cs
@@ -21,113 +21,124 @@ namespace Tagger
     /// </summary>
     public partial class PictureExplorer : Window
     {
-        public PictureExplorer(string dirPath)
+
+        List<FileInfo> files = new List<FileInfo>(); // Информация о файлах
+        public List<FileInfo> sortedFiles = new List<FileInfo>(); // Отсортированная информация о файлах
+        string path;    // Путь до сканируемой папки
+
+
+
+        public PictureExplorer(string dirPath)  // Конструктор класса
         {
-            InitializeComponent();
-            path = dirPath;
-            files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value);
+            InitializeComponent();  // Базовая инициализация компонентов
+            path = dirPath;     // Установка поля пути
+            files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value); // Сканирование директории на наличие необходимых файлов
             foreach (var cur in files)
-                sortedFiles.Add(cur);
-            //var tagLibrary = TagLib.ReadFromFile(path);
-            //var tags = TagLib.GetAllTags(tagLibrary).OrderBy(x => x);
-        }
-        public PictureExplorer()
-        {
-            InitializeComponent();
-            files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value);
-            foreach (var cur in files)
-                sortedFiles.Add(cur);
+                sortedFiles.Add(cur);   // Добавление файлов в отсортированный список
         }
 
-        int picturesInRow = 7;
 
-        List<FileInfo> files = new List<FileInfo>();
-        public List<FileInfo> sortedFiles = new List<FileInfo>();
-        string path;
-        private void Scan_Click(object sender, RoutedEventArgs e)
+
+        public PictureExplorer() : this("") // Конструктор по умолчанию
         {
-            //files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value);
-            //List<string> tagsToSearch = new List<string>();
-            files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value);
+
+        }
+
+
+
+        private void Scan_Click(object sender, RoutedEventArgs e)   // Обработчик кнопки, отвечающей за сканирование
+        {
+
+            files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value); // Сканирование директории на наличие необходимых файлов
+
             sortedFiles.Clear();
             foreach (var cur in files)
-                sortedFiles.Add(cur);      
-            var tagsToSearch = SearchBox.Text.Split(' ');
-            //if(SearchBox.Text=="")
-            foreach(var tag in tagsToSearch)
+                sortedFiles.Add(cur);   // Добавление файлов в отсортированный список
+
+            var tagsToSearch = SearchBox.Text.Split(' '); // Массив строк с необходимыми тегами
+
+            foreach(var tag in tagsToSearch) // Для каждого тега в списке
             {
                 if (tag.Length > 1)
-                    if (tag[0] == '-')
+                    if (tag[0] == '-') // Если перед тегом стоит "минус", значит надо из отсортированных файлов исключить файлы с данным тегом
                     {
                         var badFiles = sortedFiles.FindAll(x => x.FullName.Contains(tag.Substring(1)));
                         foreach (var file in badFiles)
                             sortedFiles.Remove(file);
                     }
-                    else
+                    else //Если нет "минуса", то просто добавить лишь файлы с тегом
                         sortedFiles = sortedFiles.FindAll(x => x.FullName.Contains(tag));
             }
-            if (checkMarks.Count == 0)
+            if (checkMarks.Count == 0) // Если нет отметок, то очистить грид
             {
                 grid.Children.Clear();
                 redraw();
             }
         }
 
-        protected List<string> checkedImages = new List<string>();
-        protected List<Image> checkMarks = new List<Image>();
-        protected void check(object sender, RoutedEventArgs e)
+
+
+        protected List<string> checkedImages = new List<string>(); // Список помеченных картинок (строки)
+        protected List<Image> checkMarks = new List<Image>();   // Список отметок (изображения)
+        protected void check(object sender, RoutedEventArgs e) // Обработчик нажатия на картинку (отметить картинку)
         {
-            string name = (string)((Image)sender).Tag;
-            //MessageBox.Show(name);
-            if(checkedImages.Contains(name))
+            string name = (string)((Image)sender).Tag; // Получаем
+
+            if(checkedImages.Contains(name)) // Проверяем была ли картинка уже отмечена
             {
-                checkedImages.Remove(name);
+                checkedImages.Remove(name); // Если да, удалим ее название из списка отмеченных картинок
                 grid.Children.Remove(checkMarks.Find(x => x.Margin.Left == ((Image)sender).Margin.Left && x.Margin.Top == ((Image)sender).Margin.Top));
                 checkMarks.Remove(checkMarks.Find(x => x.Margin.Left == ((Image)sender).Margin.Left && x.Margin.Top == ((Image)sender).Margin.Top));
             }
             else 
             {
-                checkedImages.Add(name);
-                Image image = new Image();
-                image.Stretch = Stretch.Fill;
-                image.Height = 10;
-                image.Width = 10;
-                image.HorizontalAlignment = HorizontalAlignment.Left;
-                image.VerticalAlignment = VerticalAlignment.Top;
-                image.Margin = new Thickness(((Image)sender).Margin.Left, ((Image)sender).Margin.Top, 0, 0);
+                checkedImages.Add(name); // Если нет, то добавим ее название в список отмеченных картинок 
+                Image image = new Image(); // Создаем новую картинку
+                image.Stretch = Stretch.Fill; // Должна заполнить область
+                image.Height = 10; // Высота
+                image.Width = 10; // Ширина
+                image.HorizontalAlignment = HorizontalAlignment.Left; // Расположена слева
+                image.VerticalAlignment = VerticalAlignment.Top;      // и сверху
+                image.Margin = new Thickness(((Image)sender).Margin.Left, ((Image)sender).Margin.Top, 0, 0); // Расположение в верхнем левом углу картинки метки
                 image.Source = checkSymbol.Source;
-                checkMarks.Add(image);
-                grid.Children.Add(checkMarks.Last());
+                checkMarks.Add(image);  // Добавление отметки в список отметок
+                grid.Children.Add(checkMarks.Last());   // Добавление метки на грид
             }
-            Transfer.PutSearchedFiles(checkedImages);
+            Transfer.PutSearchedFiles(checkedImages); // Добавление отмеченных файлов в список трансфера
         }
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+
+
+
+        int picturesInRow = 7;
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e) // Перерисовка при смене размера окна
         {
-            picturesInRow = (int)(window.Width-50) / 100;
-            if (checkMarks.Count == 0)
+            picturesInRow = (int)(window.Width-50) / 100; // Определение количества изобра
+            if (checkMarks.Count == 0) // Удаление элементов грида при отстутствии отметок (картинок) (вроде спец костыль)
             {
-                grid.Children.Clear();
-                redraw();
+                grid.Children.Clear(); // (внести в redraw)
+                redraw(); // Перерисовка
             }
         }
+
+
+
         private void redraw()
         {
-            //files = FileProcessor.ScanDirectories(path, IsInnerDirectories.IsChecked.Value);
             int lastLeftGrid = 0;
             int lastTopGrid = 0;
             int ImageNum = 0;
-            foreach (var cur in sortedFiles)
+            foreach (var cur in sortedFiles) // Для каждого файла в отсортированном списке
             {
                 try
                 {
-                    ImageNum++;
-                    BitmapImage bitmapImage = new BitmapImage();
+                    ImageNum++; // Увеличим счетчик файлов на 1
+                    BitmapImage bitmapImage = new BitmapImage(); // Создадим bitmap и инициализируем его указанной картинкой
                     bitmapImage.BeginInit();
                     bitmapImage.UriSource = new Uri(cur.FullName, UriKind.Relative);
                     bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
                     bitmapImage.EndInit();
 
-                    var image = new Image();
+                    var image = new Image(); // Создадим картинку и инициализируем ее
                     image.Stretch = Stretch.Fill;
                     image.Height = 100;
                     image.Width = 100;
@@ -135,31 +146,32 @@ namespace Tagger
                     image.VerticalAlignment = VerticalAlignment.Top;
                     image.Margin = new Thickness(lastLeftGrid, lastTopGrid, 0, 0);
                     image.MouseDown += check;
+                    image.Source = bitmapImage;
+                    image.Tag = cur.FullName;
+
                     lastLeftGrid += 100;
-                    if (ImageNum % picturesInRow == 0)
+                    if (ImageNum % picturesInRow == 0) // Если максимум картинок в ряду достигнут перейдем на нижнюю строку
                     {
                         lastTopGrid += 100;
                         lastLeftGrid = 0;
                     }
-                    image.Source = bitmapImage;
-                    image.Tag = cur.FullName;
 
-                    grid.Children.Add(image);
+                    grid.Children.Add(image); // Добавление картинки на грид
                 }
                 catch { }
             }
             foreach(var cur in checkMarks)
             {
-                grid.Children.Add(cur);
+                grid.Children.Add(cur); // Добавление меток на грид
             }
             
         }
 
-        private void SearchBox_KeyDown(object sender, KeyEventArgs e)
+        private void SearchBox_KeyDown(object sender, KeyEventArgs e) // Обработчик нажатия кнопки Enter в блоке задания тегов
         {
             if (e.Key == Key.Enter)
             {
-                if (checkMarks.Count == 0)
+                if (checkMarks.Count == 0) // Тот же костыль
                 {
                     grid.Children.Clear();
                     Scan_Click(this, e);
@@ -167,7 +179,7 @@ namespace Tagger
                 }
             }
         }
-        private void IsInnerDirectories_Checked(object sender, RoutedEventArgs e)
+        private void IsInnerDirectories_Checked(object sender, RoutedEventArgs e) // Обработка галочки на сканирование подпапок
         {
             if (checkMarks.Count == 0)
             {
